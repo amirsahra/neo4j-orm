062592afebcd6eb8688b2e13558101f9fdc5aa0a
amirsahra <amirhosein.sahra@gmail.com>
2023-03-23 11:21:19 +0330

    feat: service provider

--- src/Neo4jORMServiceProvider.php
+++ src/Neo4jORMServiceProvider.php
@@ -6,5 +6,16 @@ use Illuminate\Support\ServiceProvider;
 
 class Neo4jORMServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        parent::register();
+        $this->app->bind('Neo4jORM', function () {
+            return new Neo4jORM();
+        });
+    }
 
+    public function boot(): void
+    {
+
+    }
 }
